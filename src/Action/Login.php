cf62d6417032d723cd20ea4625a5ee4844b10b6d
huytbt <huytbt@gmail.com>
2015-03-26 17:11:39 +0700

    Implement code FlexUser\Action\Login #9

--- src/Action/Login.php
+++ src/Action/Login.php
@@ -2,13 +2,50 @@
 
 namespace FlexUser\Action;
 
+use FlexUser\Model\User;
+use FlexUser\Component\Token;
+use FlexBase\Boom;
+
 /**
- * Login action
+ * Login action.
  */
 class Login extends \FlexBase\Action
 {
-    public function run()
+    /**
+     * Run this action
+     * @param  string  $email      Email
+     * @param  string  $password   Password
+     * @param  Closure $callback   Password
+     * @return FlexUser\Model\User User model
+     */
+    public function run($email, $password, $callback)
     {
+        $before = $this->before;
+        $after = $this->after;
 
+        if ($before && ($success = $before($email, $password)) !== true) {
+            return $callback($success, null);
+        }
+
+        // find user
+        $user = User::findByEmail($email);
+
+        // compare password
+        if (!$user->comparePassword($password)) {
+            return $callback(Boom::unauthorized('Username or password does not match.'), null);
+        }
+
+        // create token
+        $token = Token::create($user);
+
+        $after && $this->after(array(
+            'user'  => $user,
+            'token' => $token,
+        ));
+
+        $callback(null, array(
+            'user'  => $user,
+            'token' => $token,
+        ));
     }
 }
