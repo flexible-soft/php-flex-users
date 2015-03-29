aba7da73ed389d13166b4df75d9d64c65ce99e67
huytbt <huytbt@gmail.com>
2015-03-29 23:13:01 +0700

    Update Test Login action #9

--- tests/yii/tests/unit/Action/LoginTest.php
+++ tests/yii/tests/unit/Action/LoginTest.php
@@ -20,6 +20,7 @@ class LoginTest extends \PHPUnit_Framework_TestCase
             if (empty($password)) {
                 return 'Password cannot be blank.';
             }
+
             return true;
         };
     }
@@ -42,9 +43,47 @@ class LoginTest extends \PHPUnit_Framework_TestCase
 
             \PHPUnit_Framework_TestCase::assertEquals($err, 'Password cannot be blank.');
         });
+    }
+
+    /**
+     * @expectedException \FlexBase\BoomUnauthorizedException
+     * @expectedExceptionCode 401
+     */
+    public function testFindUserNotFound()
+    {
+        $this->loginAction->run('nouser@example.com', '123456', function ($err, $data) {
+
+            if (!empty($err) && $err instanceof \Exception) {
+                throw $err;
+            }
+        });
+    }
+
+    /**
+     * @expectedException \FlexBase\BoomUnauthorizedException
+     * @expectedExceptionCode 401
+     */
+    public function testComparePasswordNotMatch()
+    {
+        $this->loginAction->run('test1@example.com', '123sdasdGA%@&#TGS', function ($err, $data) {
+
+            if (!empty($err) && $err instanceof \Exception) {
+                throw $err;
+            }
+        });
+    }
+
+    public function testLoginSuccess()
+    {
+        $this->loginAction->encryptionAlgorithm = function ($password) {
+
+            return $password;
+        };
+
         $this->loginAction->run('test1@example.com', 'pass1', function ($err, $data) {
 
             \PHPUnit_Framework_TestCase::assertEquals($err, null);
+            \PHPUnit_Framework_TestCase::assertEquals($data['user']->email, 'test1@example.com');
         });
     }
 }
